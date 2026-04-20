ba2ccb66309c423f2a76607250cbde31f79b4bb2
Khai Tran <khaitran@Khais-MacBook.local>
2026-04-20 20:40:29 +1000

    Add #37

--- Backtracking/37.py
+++ Backtracking/37.py
@@ -47,49 +47,49 @@ def solveSudoku(board) -> None:
                     return False
         return True
 
-    def solve(board, r, c, nums, used):
+    def solve(board, r, c, nums, used, false_boards):
         if isValidSudoku(board) and isFilled(board):
-            return
-
-        if r == 2:
-            print("Currently filled board:")
-            for t_idx, r1 in enumerate(board):
-                print(r1)
-                if t_idx == 8:
-                    print("-"*20)
-            a = 1
-
+            return True
+        
         cell = board[r][c]
         if cell == ".":
             for left_num in nums:
                 board[r][c] = left_num
-                # if board[0][5] == '6':
-                #     a = 1
-                # print("Currently filled board:")
-                # for t_idx, r1 in enumerate(board):
-                #     print(r1)
-                #     if t_idx == 8:
-                #         print("-"*20)
-                if isValidSudoku(board):
-                    increase = (c+1)//9
-                    used[r].add(left_num)
-                    outcome = solve(board, r+increase, (c+1) %
-                                    9, sorted(total_nums - used[r+increase]), used)
-                    if not outcome:
-                        board[r][c] = "."
-                        used[r].remove(left_num)
+                check_board = tuple([tuple(r) for r in board])
+                if not check_board in false_boards:
+                    if isValidSudoku(board):
+                        if not isFilled(board):
+                            increase = (c+1)//9
+                            used[r].add(left_num)
+                            outcome = solve(board, r+increase, (c+1) %
+                                            9, sorted(total_nums - used[r+increase]), used, false_boards)
+                            if not outcome:
+                                false_board = tuple([tuple(r) for r in board])
+                                if not false_board in false_boards:
+                                    false_boards.add(false_board)
+                                board[r][c] = "."
+                                used[r].remove(left_num)
+                            else:
+                                return outcome
+                        else:
+                            return True
 
+                    else:
+                        false_board = tuple([tuple(r) for r in board])
+                        false_boards.add(false_board)
+                        board[r][c] = "."
+                        next_nums = [i for i in nums if i != left_num]
+                        if len(next_nums) == 0:
+                            return False
+                        return solve(board, r, c, next_nums, used, false_boards)
                 else:
                     board[r][c] = "."
-                    next_nums = [i for i in nums if i != left_num]
-                    if len(next_nums) == 0:
-                        return False
-                    outcome = solve(board, r, c, next_nums, used)
+                    outcome = False
             return outcome
         else:
             increase = (c+1)//9
-            solve(board, r+increase, (c+1) %
-                  9, sorted(total_nums - used[r+increase]), used)
+            return solve(board, r+increase, (c+1) %
+                  9, sorted(total_nums - used[r+increase]), used, false_boards)
 
     used = []
     for row in board:
@@ -98,31 +98,35 @@ def solveSudoku(board) -> None:
             if cell != ".":
                 row_used.add(cell)
         used.append(row_used)
-
-    solve(board, 0, 0, total_nums, used)
+    false_boards = set()
+    solve(board, 0, 0, total_nums, used, false_boards)
 
 
 board = [
-    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
-    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
-    [".", "9", "8", ".", ".", ".", ".", "6", "."],
-    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
-    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
-    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-    [".", "6", ".", ".", ".", ".", "2", "8", "."],
-    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
-    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+    [".",".","9","7","4","8",".",".","."],
+    ["7",".",".",".",".",".",".",".","."],
+    [".","2",".","1",".","9",".",".","."],
+    [".",".","7",".",".",".","2","4","."],
+    [".","6","4",".","1",".","5","9","."],
+    [".","9","8",".",".",".","3",".","."],
+    [".",".",".","8",".","3",".","2","."],
+    [".",".",".",".",".",".",".",".","6"],
+    [".",".",".","2","7","5","9",".","."]
 ]
-result = [
-    ["5", "3", "4", "6", "7", "8", "9", "1", "2"],
-    ["6", "7", "2", "1", "9", "5", "3", "4", "8"],
-    ["1", "9", "8", "3", "4", "2", "5", "6", "7"],
-    ["8", "5", "9", "7", "6", "1", "4", "2", "3"],
-    ["4", "2", "6", "8", "5", "3", "7", "9", "1"],
-    ["7", "1", "3", "9", "2", "4", "8", "5", "6"],
-    ["9", "6", "1", "5", "3", "7", "2", "8", "4"],
-    ["2", "8", "7", "4", "1", "9", "6", "3", "5"],
-    ["3", "4", "5", "2", "8", "6", "1", "7", "9"]
-]
+# result = [
+#     ["5", "3", "4", "6", "7", "8", "9", "1", "2"],
+#     ["6", "7", "2", "1", "9", "5", "3", "4", "8"],
+#     ["1", "9", "8", "3", "4", "2", "5", "6", "7"],
+#     ["8", "5", "9", "7", "6", "1", "4", "2", "3"],
+#     ["4", "2", "6", "8", "5", "3", "7", "9", "1"],
+#     ["7", "1", "3", "9", "2", "4", "8", "5", "6"],
+#     ["9", "6", "1", "5", "3", "7", "2", "8", "4"],
+#     ["2", "8", "7", "4", "1", "9", "6", "3", "5"],
+#     ["3", "4", "5", "2", "8", "6", "1", "7", "9"]
+# ]
 solveSudoku(board)
-assert board == result
+print("Result:")
+for r in board:
+    print(r)
+a = 1
+# assert board == result
