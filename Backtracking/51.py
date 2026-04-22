21b64814281e007fd0d97a51df9ce739f3446d8a
Phan Khai Tran <s4626478@eait.uq.edu.au>
2026-04-22 16:29:23 +1000

    Solved #51

--- Backtracking/51.py
+++ Backtracking/51.py
@@ -9,9 +9,10 @@ def solveNQueens(n):
     board = []
     for j in range(n):
         board.append(['.' for _ in range(n)])
-    
+
     def solve(board, placed_queens_num, idx_r, rows, cols, sum_r_and_c, diff_r_and_c):
         if placed_queens_num == n:
+            final_results.append(["".join(r) for r in board])
             return True
 
         for idx_c in range(n):
@@ -25,10 +26,7 @@ def solveNQueens(n):
                 placed_queens_num += 1
 
                 if solve(board, placed_queens_num, idx_r+1, rows, cols, sum_r_and_c, diff_r_and_c):
-                    if idx_r != 0:
-                        return True
-                    else:
-                        final_results.append(board)
+                    if idx_r == 0:
                         board = []
                         for j in range(n):
                             board.append(['.' for _ in range(n)])
@@ -37,20 +35,18 @@ def solveNQueens(n):
                         sum_r_and_c = {i: set() for i in range(2*n - 1)}
                         diff_r_and_c = {i: set() for i in range(1-n, n)}
                         placed_queens_num = 0
-                else:
-                    board[idx_r][idx_c] = "."
-                    rows[idx_r].remove(queen)
-                    cols[idx_c].remove(queen)
-                    sum_r_and_c[idx_r+idx_c].remove(queen)
-                    diff_r_and_c[idx_r-idx_c].remove(queen)
-                    placed_queens_num -= 1
-        
-        return False
+                        return
+
+                board[idx_r][idx_c] = "."
+                rows[idx_r].remove(queen)
+                cols[idx_c].remove(queen)
+                sum_r_and_c[idx_r+idx_c].remove(queen)
+                diff_r_and_c[idx_r-idx_c].remove(queen)
+                placed_queens_num -= 1
 
     solve(board, 0, 0, rows, cols, sum_r_and_c, diff_r_and_c)
-    final_results = [["".join(r) for r in result] for result in final_results]
     return final_results
 
 
-n = 4
-solveNQueens(n)
+for n in range(1, 10):
+    print(len(solveNQueens(n)))
